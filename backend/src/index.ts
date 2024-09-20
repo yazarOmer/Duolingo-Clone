45f4f1a8c759ff3c223d8679ec9303654f5d191f
Ömer Yazar <omeryazar0808@gmail.com>
2024-09-20 20:31:09 +0300

    initial backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
