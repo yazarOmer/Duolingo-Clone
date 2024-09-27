d81208216bf1d83b235b5b3e704f7aac64ef6f02
Ömer Yazar <omeryazar0808@gmail.com>
2024-09-28 02:50:38 +0300

    feat: add close modal

--- frontend/src/components/QuestionHeader.tsx
+++ frontend/src/components/QuestionHeader.tsx
@@ -4,6 +4,8 @@ import { Progress } from "./ui/progress";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import { Link } from "react-router-dom";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { useState } from "react";
 
 type QuestionHeaderProps = {
   totalQuestion: number;
@@ -14,14 +16,34 @@ export const QuestionHeader = ({
   totalQuestion,
   completedLen,
 }: QuestionHeaderProps) => {
+  const [open, setOpen] = useState(false);
   const user = useSelector((state: RootState) => state.auth.user);
   return (
     <div className="w-full py-14 flex items-center justify-center gap-10">
-      <Button size="icon" variant="ghost">
-        <Link to="/learn">
-          <X />
-        </Link>
-      </Button>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger>
+          <Button size="icon" variant="ghost">
+            <X />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-sm flex flex-col items-center">
+          <img src="/mascot.svg" alt="mascot" className="mt-5" />
+          <h2 className="text-2xl font-extrabold text-zinc-800 text-center my-2">
+            Wait don't go! If you quit now you will lose all your progress
+          </h2>
+
+          <Button
+            variant="primary"
+            onClick={() => setOpen(false)}
+            className="w-full"
+          >
+            Keep learning
+          </Button>
+          <Button asChild variant="dangerOutline" className="w-full">
+            <Link to="/learn">end session</Link>
+          </Button>
+        </DialogContent>
+      </Dialog>
 
       <Progress value={Math.floor((completedLen / totalQuestion) * 100)} />
 
